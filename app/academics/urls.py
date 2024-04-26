eec72fa59c7d7df3e1dcb483e5aa3a6353ef5007
andres pantoja <andrespantoja24@gmail.com>
2024-04-26 16:00:36 -0500

    update

--- app/academics/urls.py
+++ app/academics/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = {
-    path ("1", views.index, name='index'),
+    path ("home", views.index, name='index'),
     path ("list", views.list_persons, name='list'),
 
 
